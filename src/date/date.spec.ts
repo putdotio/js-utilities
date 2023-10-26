a7f6e453ee122383e9ebb98a6b83149592fa0f58
Altay <altay@zebrastik.com>
2023-10-26 08:51:56 +0300

    feat: ensure `date` handles empty inputs

--- src/date/date.spec.ts
+++ src/date/date.spec.ts
@@ -1,4 +1,11 @@
-import { toTimeAgo, ensureUTC, formatDate, daysDiff } from './date';
+import {
+  toTimeAgo,
+  ensureUTC,
+  formatDate,
+  daysDiff,
+  getUnixTimestamp,
+  daysDiffFromNow,
+} from './date';
 
 describe('date', () => {
   beforeAll(() => {
@@ -6,18 +13,40 @@ describe('date', () => {
   });
 
   describe('ensureUTC', () => {
+    it('handles empty input', () => {
+      expect(ensureUTC()).toBe('2023-06-04T09:39:03.488Z');
+    });
+
+    it('handles Date objects', () => {
+      expect(ensureUTC(new Date('2023-06-03T18:30:03.488Z'))).toBe(
+        '2023-06-03T18:30:03.488Z'
+      );
+    });
+
     it('handles put.io API timestamps', () => {
       expect(ensureUTC('2023-06-03T18:30:03')).toBe('2023-06-03T18:30:03Z');
+    });
+
+    it('handles proper ISO 8601 timestamps', () => {
+      expect(ensureUTC('2023-06-03T18:30:03Z')).toBe('2023-06-03T18:30:03Z');
     });
   });
 
   describe('toTimeAgo', () => {
+    it('handles empty input', () => {
+      expect(toTimeAgo()).toBe('N/A');
+    });
+
     it('handles relativitization of ISO 8601 timestaps', () => {
       expect(toTimeAgo('2023-06-03T18:30:03Z')).toBe('15 hours ago');
     });
   });
 
   describe('formatDate', () => {
+    it('handles empty input', () => {
+      expect(formatDate()).toBe('N/A');
+    });
+
     it('formats a date', () => {
       expect(formatDate('2023-06-03T18:30:03Z')).toBe('June 3, 2023');
     });
@@ -29,12 +58,32 @@ describe('date', () => {
     });
 
     it('calculates the difference between two dates, in calendar days', () => {
-      expect(daysDiff('2020-02-05T16:13:28')).toBe(0);
-      expect(daysDiff('2020-02-04T06:51:43')).toBe(1);
+      expect(daysDiff('2020-02-05', '2020-02-04')).toBe(1);
+    });
+  });
+
+  describe('daysDiffFromNow', () => {
+    beforeAll(() => {
+      jest.useFakeTimers().setSystemTime(new Date('2020-02-05'));
+    });
+
+    it('calculates the difference between two dates, in calendar days', () => {
+      expect(daysDiffFromNow('2020-02-05T16:13:28')).toBe(0);
+      expect(daysDiffFromNow('2020-02-04T06:51:43')).toBe(1);
     });
 
     it('returns absolute value', () => {
-      expect(daysDiff('2020-02-06T16:13:28')).toBe(1);
+      expect(daysDiffFromNow('2020-02-06T16:13:28')).toBe(1);
+    });
+
+    it('handles empty input', () => {
+      expect(daysDiffFromNow()).toBe('N/A');
+    });
+  });
+
+  describe('getUnixTimestamp', () => {
+    it('returns a unix timestamp', () => {
+      expect(getUnixTimestamp('2023-06-03T18:30:03Z')).toBe(1685817003);
     });
   });
 });
--- src/date/date.ts
+++ src/date/date.ts
@@ -5,33 +5,48 @@ import localizedFormat from 'dayjs/plugin/localizedFormat';
 dayjs.extend(localizedFormat);
 dayjs.extend(relativeTime);
 
-type UTCTimestamp = string;
-
-export const ensureUTC = (date: string = ''): UTCTimestamp => {
-  if (!date.endsWith('Z')) {
-    date = `${date}Z`;
+export const ensureUTC = (date: unknown = null): string => {
+  if (date instanceof Date) {
+    return date.toISOString();
   }
 
-  return date;
+  if (typeof date === 'string') {
+    return date.endsWith('Z') ? date : `${date}Z`;
+  }
+
+  return new Date().toISOString();
 };
 
-export const toTimeAgo = (date: UTCTimestamp = '') => {
+export const toTimeAgo = (date: unknown = null) => {
+  if (!date) {
+    return 'N/A';
+  }
+
   return dayjs(ensureUTC(date)).fromNow();
 };
 
-export const formatDate = (date: UTCTimestamp = '', dateFormat = 'LL') => {
-  return dayjs(new Date(ensureUTC(date))).format(dateFormat);
+export const formatDate = (date: unknown = null, dateFormat = 'LL') => {
+  if (!date) {
+    return 'N/A';
+  }
+
+  return dayjs(ensureUTC(date)).format(dateFormat);
 };
 
-export const daysDiff = (
-  date1: UTCTimestamp = new Date().toISOString(),
-  date2: UTCTimestamp = new Date().toISOString()
-) => {
+export const daysDiff = (date1: unknown, date2: unknown) => {
   const day1 = dayjs(ensureUTC(date1)).startOf('day');
   const day2 = dayjs(ensureUTC(date2)).startOf('day');
   return Math.abs(day2.diff(day1, 'day'));
 };
 
-export const getUnixTimestamp = (date: UTCTimestamp = '') => {
+export const daysDiffFromNow = (date: unknown = null) => {
+  if (!date) {
+    return 'N/A';
+  }
+
+  return daysDiff(date, new Date());
+};
+
+export const getUnixTimestamp = (date: unknown) => {
   return dayjs(ensureUTC(date)).unix();
 };
